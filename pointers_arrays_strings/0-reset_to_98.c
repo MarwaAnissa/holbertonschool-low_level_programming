c07cc4deb13fcf0cb6d699eb797d21f997ea4fc0
MarwaAnissa <6736@holbertonstudents.com>
2023-06-26 13:24:27 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+int main() {
+    
+   
+int number = 42;
+
+    printf("Before: %d\n", number);
+    reset_to_98(&number);
+    
+    reset_to_98(&number
+
+    reset_to_98
+
+    reset_to
+printf("After: %d\n", number);
+
+    return 0;
+}
